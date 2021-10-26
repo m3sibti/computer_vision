60199dc6b2b32398aa7bb22d646959d4b13d9e3e
m3sibti <m3sibti@gmail.com>
2021-10-26 18:21:50 +0500

    t001 load images

---
+++ c001_opencv_basics/t001_opencv.py
@@ -0,0 +1,18 @@
+import cv2
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--input_img', required=True, help='Path to the input image')
+ap.add_argument('-o', '--output_img', required=True, help='Path to the output image')
+args = vars(ap.parse_args())
+
+img = cv2.imread(args['input_img'])
+(h, w, c) = img.shape[:3]
+
+print(f'Height: {h}')
+print(f'Width: {w}')
+print(f'Channels: {c}')
+
+cv2.imshow('Window', img)
+cv2.waitKey(0)
+cv2.imwrite(args['output_img'], img)
